b218a4649e6eaaca8c3a5fcd081c3013086af5c3
Yaxin Cheng <yaxin.cheng@icloud.com>
2020-06-03 12:43:09 -0400

    Initial Commit

---
+++ VolumeEjectService/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VolumeEjectService
+//
+//  Created by Yaxin Cheng on 2020-06-03.
+//  Copyright © 2020 Yaxin Cheng. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
